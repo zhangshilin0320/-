faea646e9707fcd4d9a5bbe800143e2dfda7cf43
zhangshilin <zhangshilin66@126.com>
2020-06-04 11:17:18 +0800

    项目的初始化1

--- pet-user/per-user-service/src/main/java/com/pet/user/mapper/UserMapper.java
+++ pet-user/per-user-service/src/main/java/com/pet/user/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.pet.user.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pet.user.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
 
 
 public interface UserMapper extends BaseMapper<User> {
